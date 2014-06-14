c83d7eebdb3a8fb04bfccbee7550f85f6ba32d59
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-14 19:33:53 +0100

    Wohoo, I think i've cracked the oracle database installation!

--- cookbooks/oracle_instantclient/recipes/default.rb
+++ cookbooks/oracle_instantclient/recipes/default.rb
@@ -1,3 +1,7 @@
+package "libaio1" do
+  action :install
+end
+
 cookbook_file node['oracle_instantclient']['basic_zip'] do
   path File.join(Chef::Config[:file_cache_path], node['oracle_instantclient']['basic_zip'])
   action :create_if_missing
@@ -23,6 +27,7 @@ end
 
 execute "Move the files" do
   command "mv /tmp/#{node['oracle_instantclient']['folder_name']} /usr/lib/oracle/#{node['oracle_instantclient']['version']}/client64/lib"
+  not_if do FileTest.directory?("/usr/lib/oracle/#{node['oracle_instantclient']['version']}/client64/lib") end
 end
 
 link "/usr/lib/oracle/#{node['oracle_instantclient']['version']}/client64/lib/libclntsh.so" do
